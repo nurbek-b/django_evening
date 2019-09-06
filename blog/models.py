d96c4fff93eefad11fa528a352b9689e5a4a9eb1
Nurbek <foto.nurbek@gmail.com>
2019-09-06 18:16:26 +0600

    First commit

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User',
+                               on_delete=models.CASCADE)
+    body = models.TextField()
+    photo = models.ImageField(upload_to='images/', null=True, blank=True)
+    date = models.DateTimeField(auto_now=datetime.now)
+
+    def __str__(self):
+        return self.title
